039b42d1c330aeff8d35c712da94d2f74c6934ab
David North <bigchimp2005+github@gmail.com>
2012-11-06 21:58:36 +0000

    Adds the first level
    Adds the first level JSON string.. it begins

---
+++ js/lib/levels/level_1.js
@@ -0,0 +1,7 @@
+[
+{"type":"wall", "x":0, "y":0, "repeat-y":20},
+{"type":"wall", "x":48, "y":0, "repeat-x":20},
+{"type":"wall", "x":0, "y":960, "repeat-x":20},
+{"type":"wall", "x":912, "y":0, "repeat-y":20},
+{"type":"player", "x":150, "y": 900}
+]
