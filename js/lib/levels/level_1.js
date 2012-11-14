9f3bbc28fab9acfc405f2bfb4b8073d4d9b00701
David North <bigchimp2005+github@gmail.com>
2012-11-14 20:31:21 +0000

    Removes the uneeded level JSON and replaces some values to ensure the correct placing of objects

--- js/lib/levels/level_1.js
+++ js/lib/levels/level_1.js
@@ -1,11 +1,9 @@
 [
 {"type":"stats","clonePar":1,"timePar":30},
-{"type":"wall","x":0,"y":0,"repeat-y":12},
-{"type":"wall","x":0,"y":576,"repeat-x":20},
-{"type":"wall","x":960,"y":0,"repeat-y":12},
-{"type":"player","x":150,"y": 500},
-{"type":"lever","x":350,"y": 512,"outputs":[{"type":"door", "x":800,"y":277}]},
+{"type":"wall","x":0,"y":330,"repeat-x":2},
+{"type":"player","x":150,"y": 200},
+{"type":"lever","x":350,"y": 289,"outputs":[{"type":"door", "x":700,"y":-134}]},
 {"type":"tooltip","x":350,"y":512,"width":100,"height":100,"text":"Woo!"},
 {"type":"tooltip","x":390,"y":500,"width":100,"height":100,"text":"Woo 2!"},
-{"type":"goal","x":890,"y":525}
+{"type":"goal","x":890,"y":188}
 ]
